09ead2fdc3d40fefa138dfc5e1be6c6a06de12ac
Vinokurov Kirill <vinokurov@elewise.com>
2018-04-12 15:49:31 +0400

    develop

--- model/IEntity.h
+++ model/IEntity.h
@@ -1,13 +1,21 @@
 #ifndef IENTITY_H
 #define IENTITY_H
 
+#include <QObject>
 #include <core/types/Guid.h>
 
 class IEntity
 {
+    Q_OBJECT
+
+    Q_PROPERTY(Guid Id READ getId WRITE setId)
+
 public:
+    Guid getId() { return _id; }
+    void setId(Guid value) { _id = value; }
 
-
+private:
+    Guid _id;
 };
 
 #endif // IENTITY_H
