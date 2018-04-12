9f1f1bbe3bec897ae147f35399d24d48d55702d3
Vinokurov Kirill <vinokurov@elewise.com>
2018-04-12 15:39:36 +0400

    develop Guid

--- core/types/Guid.cpp
+++ core/types/Guid.cpp
@@ -6,17 +6,25 @@ Guid::Guid(const Guid &other)
     _data = other._data;
 }
 
-Guid &Guid::newGuid()
+Guid Guid::newGuid()
 {
-
+    Guid ret;
+    quint8 *ptr = (quint8 *)&(ret._data);
+    for (int i = 0; i < sizeof(GuidStructure); i++)
+    {
+         ptr[i] = RandomHelper::Instance().nextInt(0x100);
+    }
+    return ret;
 }
 
-Guid &Guid::emptyGuid()
+Guid Guid::emptyGuid()
 {
-
+    Guid ret;
+    memset(&ret._data, 0, sizeof(GuidStructure));
+    return ret;
 }
 
-Guid &Guid::parse(QString guid, bool *ok)
+Guid Guid::parse(QString guid, bool *ok)
 {
 
 }
@@ -44,6 +52,17 @@ bool Guid::operator <(const Guid &other)
 bool Guid::operator >(const Guid &other)
 {
 
+}
+
+QString Guid::toString()
+{
+    return QString("%1-%2-%3-%4-%5%6").arg(
+                QString::number(_data.Data1, 16).rightJustified(8, '0'),
+                QString::number(_data.Data2, 16).rightJustified(4, '0'),
+                QString::number(_data.Data3, 16).rightJustified(4, '0'),
+                QString::number((quint16)((_data.Data4 & 0xffff0000) >> 16), 16).rightJustified(4, '0'),
+                QString::number((quint16)( _data.Data4 & 0x0000ffff), 16).rightJustified(4, '0'),
+                QString::number( _data.Data5, 16).rightJustified(8, '0'));
 }
 
 Guid::Guid()
--- core/types/Guid.h
+++ core/types/Guid.h
@@ -18,25 +18,27 @@ public:
     /// \brief newGuid создает новый Guid
     /// \return
     ///
-    static Guid &newGuid();
+    static Guid newGuid();
     ///
     /// \brief emptyGuid возвращает пустой Guid
     /// \return
     ///
-    static Guid &emptyGuid();
+    static Guid emptyGuid();
     ///
     /// \brief parse получает Guid из строкового значения. Если преобразование не удалось, то вернется пустой Guid.
     /// \param guid строка, которую необходимо распарсить
     /// \param ok результат обработки
     /// \return
     ///
-    static Guid &parse(QString guid, bool *ok = nullptr);
+    static Guid parse(QString guid, bool *ok = nullptr);
 
     bool operator !=(const Guid &other);
     bool operator ==(const Guid &other);
     Guid &operator =(const Guid &other);
     bool operator <(const Guid &other);
     bool operator >(const Guid &other);
+
+    QString toString();
 
 
 private:
